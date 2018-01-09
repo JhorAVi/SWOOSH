c92eb245315536cf8792104f1a0b74480b4ebf58
jhoravi <jhoravi@gmail.com>
2018-01-09 09:27:51 +0800

    Finished included saved state

--- app/src/main/java/com/example/jhoravi/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/jhoravi/swoosh/Controller/SkillActivity.kt
@@ -2,6 +2,7 @@ package com.example.jhoravi.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.jhoravi.swoosh.Model.Player
@@ -12,12 +13,24 @@ import kotlinx.android.synthetic.main.activity_skill.*
 class SkillActivity : BaseActivity() {
     lateinit var player : Player
 
+    // save instance
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         player = intent.getParcelableExtra(EXTRA_PLAYER)
+    }
 
-       // println("skill testing $league")
+    // load instance
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState != null){
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onBeginnerClicked(view: View) {
